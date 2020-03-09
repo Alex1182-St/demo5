ea1cef7a3b8d51f4afa855828959be45e96ae7a2
OlekSt <alexander.stepaniuk@yahoo.com.ua>
2020-03-09 12:48:04 +0200

    Training with Kotlin and GraphQL

--- src/main/kotlin/com/example/demo5/UserMutationResolver.kt
+++ src/main/kotlin/com/example/demo5/UserMutationResolver.kt
@@ -2,6 +2,7 @@ package com.example.demo5
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Component
+import java.util.*
 
 
 @Component
@@ -10,5 +11,8 @@ class UserMutationResolver (val userServ: UserService) : GraphQLQueryResolver{
 fun createUser (userName: String, userSurname: String, age: Int?, email: String?): UserEntity {
     return userServ.createUser(userName,userSurname,age,email)
 }
+fun deleteUser (id: UUID) {
+    return userServ.deleteUser(id)
+    }
 
 }
